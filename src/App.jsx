2bfbb4af9539b13a8485485b9ef06d182d81d9bf
AkashMAbrrar <madborakash48@gmail.com>
2024-11-11 17:40:29 +0600

    this is the first commit for added all the files with react router dom combined with firebase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-3xl text-center">
+        Here we go with tailwind daisy router authentication.
+      </h2>
+      <button className="btn btn-primary">Click me</button>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Firebase/Firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBOvYhlTRef06riN1DNZIr1_XK1vUAVW-Y",
+  authDomain: "fir-rotuerproject.firebaseapp.com",
+  projectId: "fir-rotuerproject",
+  storageBucket: "fir-rotuerproject.firebasestorage.app",
+  messagingSenderId: "138901459448",
+  appId: "1:138901459448:web:76ab46892456a28222060d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
